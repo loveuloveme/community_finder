e5a564d0e0505d4162edf7755a03aa83510affd9
Oleg Podkolzin <olegpodkolzin@icloud.com>
2020-05-26 21:09:28 +0300

    Updates

---
+++ precache-manifest.173e7d87b5f4dd25af918f4b884bc061.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25c17e975b817730dfd4bbc12c56f54b",
+    "url": "/community_finder/index.html"
+  },
+  {
+    "revision": "28417a8f8b6bc5ba3c5c",
+    "url": "/community_finder/static/css/main.c021fc5b.chunk.css"
+  },
+  {
+    "revision": "78714e1fb637d4b9a3c0",
+    "url": "/community_finder/static/js/2.34e3dbd2.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/community_finder/static/js/2.34e3dbd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28417a8f8b6bc5ba3c5c",
+    "url": "/community_finder/static/js/main.09eae0d0.chunk.js"
+  },
+  {
+    "revision": "29f79b8b726f6110a736",
+    "url": "/community_finder/static/js/runtime-main.96d880e4.js"
+  },
+  {
+    "revision": "c57f70ad42aad3300e9955ef1ce44761",
+    "url": "/community_finder/static/media/exm.c57f70ad.png"
+  },
+  {
+    "revision": "901cb84e7903d5694272b8d8065cc047",
+    "url": "/community_finder/static/media/load.901cb84e.gif"
+  }
+]);
